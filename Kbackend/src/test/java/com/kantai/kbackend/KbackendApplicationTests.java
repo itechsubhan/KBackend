ccbed1886271e4572a6d2927bb8b29bed9038956
itechsubhan <subhanitech63@gmail.com>
2023-12-24 00:31:31 +0530

    Spring boot init

---
+++ Kbackend/src/test/java/com/kantai/kbackend/KbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kantai.kbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
